475016a33312b148d85f928e71ef808ae8b290ea
Ardak Tokzhan <atqwer@Air-Ardak.mshome.net>
2019-02-15 02:01:24 +0600

    Initial Commit

---
+++ Week1/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // reading the array size
+            int[] a = new int[n + 1]; // creted a 1-indexed array, uses only a+1..a+n+1
+            for (int i = 1; i <= n; i++)
+            {   // reading the array integers
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(a[i].ToString() + " " + a[i].ToString() + " "); // convert a[i] to string and print them twice with spaces separated between
+            }
+        }
+
+    }
+}
---
+++ Week1/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // reading n
+            for (int i = 1; i <= n; i++)
+            { // iterarting over rows
+                for (int j = 1; j <= i; j++)
+                { // iterating over columns
+                    Console.Write("[*]"); // printing a block
+                }
+                Console.WriteLine(); // moving to the next line(row) in console
+            }
+        }
+    }
+}
